92871949be70bd24f839af9cfd9d0480c713476e
Bruno Tarditi <brunotarditi@hotmail.com>
2021-06-28 00:12:23 -0300

    Configurando Cors

--- instrumentos-api/Startup.cs
+++ instrumentos-api/Startup.cs
@@ -27,6 +27,7 @@ namespace instrumentos_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
 
             services.AddDbContext<instrumentos_apiContext>(options =>
@@ -36,6 +37,12 @@ namespace instrumentos_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:4200");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
